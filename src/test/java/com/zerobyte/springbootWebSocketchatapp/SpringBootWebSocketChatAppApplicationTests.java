82cb1b7a4288a095016287b6a53f88571ae64fd3
java5880 <85115457+java5880@users.noreply.github.com>
2021-07-29 19:39:23 +0530

    Add files via upload

---
+++ src/test/java/com/zerobyte/springbootWebSocketchatapp/SpringBootWebSocketChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobyte.springbootWebSocketchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebSocketChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
